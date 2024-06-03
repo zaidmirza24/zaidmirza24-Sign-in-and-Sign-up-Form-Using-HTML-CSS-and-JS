9d0217556cc3fe65e359d6afd2e09690711f16ed
Mohd Zaid Mirza <143643546+zaidmirza24@users.noreply.github.com>
2024-06-03 06:18:09 +0000

    Add Transition to input Name field.

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-console.log("Hey, Iam Zaid Mirza")
+console.log("Hey, I am Zaid Mirza");
 
-let signin = document.getElementById('signinbtn')
-let signup = document.getElementById('signupbtn')
-let namefield = document.querySelector('.input-field')
-let h1 = document.querySelector('.heading')
+let signin = document.getElementById('signinbtn');
+let signup = document.getElementById('signupbtn');
+let namefield = document.getElementById('name-field');
+let h1 = document.querySelector('.heading');
 
-    // signin.addEventListener('click',function(){
-    //     signup.style.backgroundColor = 'white';
-    // })
-    // signup.addEventListener('click',function(){
-    //     signin.style.backgroundColor = 'white';
-    // })
+signin.addEventListener('click', () => {
+    namefield.classList.add('hidden');
+    h1.innerHTML = 'Sign in';
+    signin.style.backgroundColor = '#A90BC9';
+    signup.style.backgroundColor = 'white';
+});
 
-signin.addEventListener('click',()=>{
-    namefield.style.display = 'none';
-    h1.innerHTML = `Sign in`
-    if(signup.style.backgroundColor != '#A90BC9'){
-        console.log('inside if')
-        signin.style.backgroundColor = '#A90BC9'
-        signup.style.backgroundColor = 'white'
-    }
-})
-signup.addEventListener('click',()=>{
-    namefield.style.display = 'flex';
-    h1.innerHTML = `Sign up`
-    if(signin.style.backgroundColor != '#A90BC9'){
-        console.log('inside if')
-        signup.style.backgroundColor = '#A90BC9'
-        signin.style.backgroundColor = 'white'
-        
-    }
-})
-
+signup.addEventListener('click', () => {
+    namefield.classList.remove('hidden');
+    h1.innerHTML = 'Sign up';
+    signup.style.backgroundColor = '#A90BC9';
+    signin.style.backgroundColor = 'white';
+});
